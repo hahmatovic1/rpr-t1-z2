cd434f2233f3bffe9784da3b491024d540a84ca1
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-10-30 18:22:51 +0100

    Inicijalni komit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int sumaCifara(int n) {
+        int suma = 0;
+        while(n != 0){
+            suma = suma + n%10;
+            n = n / 10;
+        }
+       return suma;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Unesite prirodan broj: ");
+        int n;
+	    Scanner in = new Scanner(System.in);
+	    n = in.nextInt();
+
+	    for(int i = 1; i <= n; i++){
+	        if((i % sumaCifara(i)) == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+
+}
